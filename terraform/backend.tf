2e786924fb79e3d25207782078909e5452ef8759
codebydeep <deepanshvish123@gmail.com>
2026-09-18 17:24:41 +0530

    added- tf-backend-state

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "transdoo-terraform-state"
-    key          = "ec2/terraform.tfstate"
-    region       = "ap-southeast-2"
-    encrypt      = true
+    bucket  = "transdoo-terraform-state"
+    key     = "ec2/terraform.tfstate"
+    region  = "ap-southeast-2"
+    encrypt = true
     # use_lockfile = true
   }
 }
